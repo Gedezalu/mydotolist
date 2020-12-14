dc9482af719bfe83e8dd74abf53638efe3e3064c
Gedezalu <72193750+Gedezalu@users.noreply.github.com>
2020-12-14 09:32:02 +0100

    Page de structure

---
+++ nouveauclient.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+    <input type="text" id="pseudo" name="pseudo" placeholder="Entrez votre pseudonyme"><br>
+    <input type="text" id="mail" name="mail" placeholder="Entrez votre adresse e-mail"><br>
+    <input type="text" id="mail2" name="mail2" placeholder="Confirmez votre adresse e-mail"><br>
+    <input type="text" id="motdepasse" name="motdepasse" placeholder="Entrez votre mot de passe"><br>
+    <input type="text" id="motdepasse2" name="motdepasse2" placeholder="Confirmez votre mot de passe"><br>
+    <button> Validez votre isncription</button>
+    </form>
+</body>
+</html>
